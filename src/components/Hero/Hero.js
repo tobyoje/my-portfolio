0ec5fb69305e72d39488dfed4201db3ff8894e69
Oluwatobi Ojelabi <tobi.oje@icloud.com>
2023-06-04 06:40:57 +0100

    Setting up components and contents

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,7 +1,32 @@
+import "./Hero.scss";
+import TobyIMG from "../../assets/images/toby.png";
+
 const Hero = () => {
   return (
     <>
-      <h2>This is my hero Component</h2>
+      <div className="hero">
+        <div className="hero-inner">
+          <div className="hero1">
+            <img className="hero__image" src={TobyIMG} alt="Toby" />
+          </div>
+          <div className="hero2">
+            <h2 className="hero__heading">Oluwatobi Ojelabi</h2>
+            <p className="hero__subtitle">Software Engineer & UI/UX Designer</p>
+          </div>
+        </div>
+
+        <p className="hero__bio">
+          I am a creative thinker and a problem solver. I convert complex human
+          problems into basic simple solutions with design and codes applying my
+          knowledge and skills towards continuous improvement of self, people
+          and brands.
+        </p>
+
+        <p className="hero__stacks">
+          HTML5, CSS, JavaScript, React.js, Node.js, Express.js, Web APIs,
+          MySQL, Figma, Sketch, Adobe Photoshop, Wordpress, Webflow.
+        </p>
+      </div>
     </>
   );
 };
--- src/components/ProjectList/ProjectList.js
+++ src/components/ProjectList/ProjectList.js
@@ -1,7 +1,36 @@
+import "./ProjectList.scss";
+import workIMG from "../../assets/images/works/purple-care.jpg";
+
 const ProjectList = () => {
   return (
     <>
-      <h2>Project list goes in here</h2>
+      <div className="project-list">
+        <h2 className="project-list__heading">Some of my works </h2>
+
+        <div className="works-box">
+          <div className="work-box">
+            <img className="work-img" src={workIMG} alt="" />
+            <div className="work-info">
+              <p className="work-info__name">Purple Care Solutions</p>
+              <p className="work-info__desc">Web Design & Brand Identity</p>
+              <a className="work-info__link" href="#">
+                View Project
+              </a>
+            </div>
+          </div>
+
+          <div className="work-box">
+            <img className="work-img" src={workIMG} alt="" />
+            <div className="work-info">
+              <p className="work-info__name">Purple Care Solutions</p>
+              <p className="work-info__desc">Web Design & Brand Identity</p>
+              <a className="work-info__link" href="#">
+                View Project
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
